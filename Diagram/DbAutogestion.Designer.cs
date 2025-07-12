4e6c3cede687fb6856ef82a1e535fbfe40ad9b28
Reyving Ramirez <medinafabriany@gmail.com>
2025-07-12 07:58:47 -0500

    edicion de MER

--- Diagram/DbAutogestion.Designer.cs
+++ Diagram/DbAutogestion.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ReyvingF\OneDrive\Escritorio\Auto-gestion-revision\Diagram\DbAutogestion.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ReyvingF\Desktop\Auto-gestion-revision\Diagram\DbAutogestion.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
